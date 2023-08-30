b664a52fc016b8c8a0e86c1cf9e380e07d7ba045
mlgcreations <78622657+mlgcreations@users.noreply.github.com>
2023-08-29 22:34:32 -0500

    Create ejercicio2.java

---
+++ ejercicio2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 5; 
+
+        if (num > 0 && num % 2 != 0) {
+            System.out.println("true");
+        }
+    }
+}
