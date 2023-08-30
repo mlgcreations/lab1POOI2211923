23f785eacdf28179349fcddc8aa2f154c5f4e0fb
mlgcreations <78622657+mlgcreations@users.noreply.github.com>
2023-08-29 22:35:07 -0500

    Create ejercicio3.java

---
+++ ejercicio3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numA = 5; 
+        int numB = 10;
+
+        if (numA > 0 && numB > 0) {
+            System.out.println("true");
+        }
+    }
+}
